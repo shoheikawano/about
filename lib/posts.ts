126efb91f3b8e212e13c4f54d02c17c96d12face
shoheikawano <mail.shoheikawano@gmail.com>
2021-11-21 13:33:55 +0900

    Replace js -> ts/tsx

--- lib/posts.ts
+++ lib/posts.ts
@@ -23,17 +23,15 @@ export function getSortedPostsData() {
     // Combile the data with the id
     return {
       id,
-      ...matterResult.data
+      ...(matterResult.data as { date: string; title: stirng })
     }
   })
   // Sort posts by date
-  return allPostsData.sort(({ date:a }, { date:b }) => {
-    if (a < b) {
+  return allPostsData.sort((a, b) => {
+    if (a.date < b.date) {
       return 1
-    } else if (a > b) {
-      return -1
     } else {
-      return 0
+      return -1
     }
   })
 }
@@ -63,7 +61,7 @@ export function getAllPostIds() {
   })
 }
 
-export async function getPostData(id) {
+export async function getPostData(id: string) {
   const fullPath = path.join(postsDirectory, `${id}.md`)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
 
@@ -80,7 +78,7 @@ export async function getPostData(id) {
   return {
     id,
     contentHtml,
-    ...matterResult.data
+    ...(matterResult.data as { date: string; title: string }) 
   }
 }
 
